32281403ff2f4146b913f77b9c7d110bb7581333
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2022-08-02 15:22:57 +0200

    A

--- Functions.lua
+++ Functions.lua
@@ -1,12 +1,12 @@
 --welcome to lua functions do not add this file to your project add this code to your main script
 --this only works for lua put all these lines of code on top of your script
 
-local function wait(duration)
+local function wait(duration) --wait
     local start = os. time()
     repeat until os. time() > start + duration
 end
 
-local function printTimer(seconds)
+local function printTimer(seconds) --timer
     for i=1,seconds do
         print(seconds - i)
         wait(1)
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Extra-Functions-LUA-
 
 Extra functions is a line of code you need to put on top of your script and this will add more functions like wait() and more
+
